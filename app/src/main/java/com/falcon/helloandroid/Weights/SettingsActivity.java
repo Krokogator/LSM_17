40d1a69324960f0dd3639fc698561761f92beba6
Krokogator <michal26262@gmail.com>
2017-12-06 19:17:56 +0100

    Weight units without headers

--- app/src/main/java/com/falcon/helloandroid/Weights/SettingsActivity.java
+++ app/src/main/java/com/falcon/helloandroid/Weights/SettingsActivity.java
@@ -1,19 +1,47 @@
 package com.falcon.helloandroid.Weights;
 
+import android.app.Activity;
+import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
 
 import com.falcon.helloandroid.R;
 
 import java.util.List;
-
+/*
 public class SettingsActivity extends PreferenceActivity {
     @Override
     public void onBuildHeaders(List<Header> target) {
-        loadHeadersFromResource(R.xml.headers_settings, target);
+        loadHeadersFromResource(R.xml.weight_settings, target);
     }
 
     @Override
     protected boolean isValidFragment(String fragmentName) {
         return SettingsFragment.class.getName().equals(fragmentName);
     }
+}*/
+
+public class SettingsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Display the fragment as the main content.
+        getFragmentManager().beginTransaction().replace(android.R.id.content,
+                new PrefsFragment()).commit();
+    }
+
+
+    public static class PrefsFragment extends PreferenceFragment {
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            // Load the preferences from an XML resource
+            addPreferencesFromResource(R.xml.weight_settings);
+        }
+    }
+
 }
