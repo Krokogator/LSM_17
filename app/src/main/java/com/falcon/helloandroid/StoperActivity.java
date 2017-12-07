fcbdd4b700c2247c813b097e8bcb97f3db01efb2
Krokogator <michal26262@gmail.com>
2017-12-07 09:33:58 +0100

    Saving stopwatch state

--- app/src/main/java/com/falcon/helloandroid/StoperActivity.java
+++ app/src/main/java/com/falcon/helloandroid/StoperActivity.java
@@ -1,6 +1,7 @@
 package com.falcon.helloandroid;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -12,6 +13,8 @@ import java.util.Calendar;
 import java.util.Date;
 
 public class StoperActivity extends AppCompatActivity {
+
+    String SHARE_PREFS = "";
 
     private Date initialTime = new Date();
     private TextView tvStoper;
@@ -32,14 +35,15 @@ public class StoperActivity extends AppCompatActivity {
         clearButton = (Button) findViewById(R.id.lapClearButton);
         lapList = (TextView) findViewById(R.id.lapView);
         lapButton = (Button) findViewById(R.id.lapButton);
-        isRunning = false;
-        paused = false;
 
+        paused=true;
+        isRunning=false;
     }
 
     public void onClickStart(View view){
         if(paused) {
             paused = false;
+            this.initialTime.setTime(Calendar.getInstance().getTime().getTime() - timeOffset);
         }
         else{
             this.initialTime = Calendar.getInstance().getTime();
@@ -79,7 +83,7 @@ public class StoperActivity extends AppCompatActivity {
     };
 
     private void updateStoper(){
-        long diff = Calendar.getInstance().getTime().getTime() - initialTime.getTime() + timeOffset;
+        long diff = Calendar.getInstance().getTime().getTime() - initialTime.getTime();
         long milis = diff / 10 % 100;
         long seconds = diff / 1000 % 60;
         long minutes = diff / (60 * 1000) % 60;
@@ -92,7 +96,8 @@ public class StoperActivity extends AppCompatActivity {
     public void onClickClear(View view){
         if(isRunning){
             isRunning = false;
-            timeOffset = Calendar.getInstance().getTime().getTime() - this.initialTime.getTime() + timeOffset;
+            paused = true;
+            timeOffset = Calendar.getInstance().getTime().getTime() - this.initialTime.getTime();
             clearButton.setText("Clear");
         }
         else{
@@ -114,6 +119,29 @@ public class StoperActivity extends AppCompatActivity {
         lapList.setText("");
     }
 
+    public void onStop(){
+        super.onStop();
+        SharedPreferences saved = getApplicationContext().getSharedPreferences(SHARE_PREFS, 0);
+        SharedPreferences.Editor editor = saved.edit();
+        editor.putLong("timeOffset", timeOffset);
+        editor.putString("laps", lapList.getText().toString()); //FIX THIS!!!
+        //editor.putBoolean("isRunning", isRunning);
+        //editor.putBoolean("isPaused", paused);
+        editor.apply();
+    }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        SharedPreferences saved = getApplicationContext().getSharedPreferences(SHARE_PREFS, 0);
+        this.timeOffset = saved.getLong("timeOffset", 0);
+        this.lapList.append(saved.getString("laps", ""));
+        this.initialTime.setTime(Calendar.getInstance().getTime().getTime() - timeOffset);
+        //this.isRunning = saved.getBoolean("isRunning", false);
+        //this.paused = saved.getBoolean("isPaused", true);
+
+
+        updateStoper();
+    }
 
 }
